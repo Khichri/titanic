7abd8ef63533a1b39ca14c1aab368d3bcaf0d78f
shahank42 <crazykavlogs@gmail.com>
2023-09-09 13:41:41 +0530

    files and folders separated

--- src/components/FileManager.jsx
+++ src/components/FileManager.jsx
@@ -3,13 +3,16 @@ import FileListItem from "./FileListItem";
 
 
 const FileManager = ({ data }) => {
+  const directories = data.filter((item) => item.type === 'directory');
+  const files = data.filter((item) => item.type === 'file');
+
   return (
     <div className="w-full h-full m-auto pt-0 pb-10 px-4 bg-gray-100 rounded-lg bg-transparent overflow-y-scroll">
       <div className="sticky top-0  bg-[#3c3836]">
-        <h1 className="text-[2rem] font-['Comfortaa']  font-semibold mb-6">File Manager</h1>
+        <h1 className="text-[2rem] font-['Comfortaa'] shadow-lg py-2 font-semibold mb-6">File Manager</h1>
       </div>
       <ul className="grid grid-cols-7 h-full">
-        {data.map((item, index) => (
+        {([...directories, ...files]).map((item, index) => (
           <FileListItem key={index} item={item} />
         ))}
       </ul>
